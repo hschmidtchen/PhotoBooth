ab4ce393b2160032b88cf9aa0e6f1b37920667c4
hschmidtchen <hschmidtchen@googlemail.com>
2017-04-01 17:45:02 +0200

    split command

--- photobooth/photobooth.py
+++ photobooth/photobooth.py
@@ -64,7 +64,7 @@ def printing():
     global sessions
     #image_path = '/photos/full/print'+str(sessions)+'.jpg'
     image_path = 'photos/print/def_print.jpg'
-    cmd='./canon-selphy-print/print-selphy-card '+image_path
+    cmd=["./canon-selphy-print/print-selphy-card", str(image_path)]
     print(cmd)
     subprocess.call(cmd)
     return redirect('/')
