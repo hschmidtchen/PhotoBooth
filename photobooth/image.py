7241f9ef0c1e65296dfd444275c4586fe0cd316d
hschmidtchen <hschmidtchen@googlemail.com>
2017-04-14 15:35:04 +0200

    import for image.py

--- photobooth/image.py
+++ photobooth/image.py
@@ -15,8 +15,7 @@ class Image:
             camera.start_preview(fullscreen=False, window=(0,0,800,480), crop=(0,195,2592,1556))
             sleep(5)
             camera.capture(filename)
-            dlr_filename="/home/pi/photobooth/photobooth/static/photos/full/dlr_image2.jpg"
-            cmd = "gphoto2 --capture-image-and-download --filename "+dlr_filename
+            cmd = "gphoto2 --capture-image-and-download --filename /home/pi/photobooth/photobooth/static/photos/full/dlr_image2.jpg"
             print(cmd)
             subprocess.call(cmd)
             camera.stop_preview()
