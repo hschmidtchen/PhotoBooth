7006076042a3c183fec67ac67c95f8833aa31151
hschmidtchen <hschmidtchen@googlemail.com>
2017-04-19 11:07:38 +0200

    string utf8

--- photobooth/image.py
+++ photobooth/image.py
@@ -12,17 +12,17 @@ class Image:
         """
         with picamera.PiCamera() as camera:
             camera.resolution = (2592, 1944)
-            camera.annotate_text_size = 40
+            camera.annotate_text_size = 160
+            camera.start_preview(fullscreen=False, window=(0, 0, 800, 480), crop=(0, 195, 2592, 1556))
             for i in range(5):
                 text="%s", 5-i
                 camera.annotate_text = str(text).encode('utf_8')
-                camera.start_preview(fullscreen=False, window=(0,0,800,480), crop=(0,195,2592,1556))
                 sleep(1)
-                camera.stop_preview()
             picam_path='static/photos/full/image_%s_%s.jpg' % (session_id,photo_id)
             camera.capture(picam_path)
             dlr_path="/home/pi/photobooth/photobooth/static/photos/full/dlr_image_%s_%s.jpg" % (session_id,photo_id)
             cmd = ["gphoto2", "--capture-image-and-download", "--filename", dlr_path]
             subprocess.call(cmd)
+            camera.stop_preview()
 
         return picam_path
