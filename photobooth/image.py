99eda12f5d20eb1523b209dfb1ccd7cbcc034d53
hschmidtchen <hschmidtchen@googlemail.com>
2017-04-19 10:57:08 +0200

    url_for import

--- photobooth/image.py
+++ photobooth/image.py
@@ -12,13 +12,16 @@ class Image:
         """
         with picamera.PiCamera() as camera:
             camera.resolution = (2592, 1944)
-            camera.start_preview(fullscreen=False, window=(0,0,800,480), crop=(0,195,2592,1556))
-            sleep(5)
+            camera.annotate_text_size = 40
+            for i in range(5):
+                camera.annotate_text = '%s', 5-i
+                camera.start_preview(fullscreen=False, window=(0,0,800,480), crop=(0,195,2592,1556))
+                sleep(1)
+                camera.stop_preview()
             picam_path='static/photos/full/image_%s_%s.jpg' % (session_id,photo_id)
             camera.capture(picam_path)
             dlr_path="/home/pi/photobooth/photobooth/static/photos/full/dlr_image_%s_%s.jpg" % (session_id,photo_id)
             cmd = ["gphoto2", "--capture-image-and-download", "--filename", dlr_path]
             subprocess.call(cmd)
-            camera.stop_preview()
 
         return picam_path
