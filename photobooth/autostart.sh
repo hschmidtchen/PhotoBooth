0194816dac55e8a5e8b53a85d6cb80892cb61e3a
hschmidtchen <hagen.schmidtchen@gmail.com>
2017-10-20 09:32:11 +0200

    autostart update

--- photobooth/autostart.sh
+++ photobooth/autostart.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 cd photobooth/photobooth
 
+#disable mouse pointer
+unclutter -idle 0.05 -root &
+
+#clear CUPS queue
+cancel -a -
+
+#start flask app
 python photobooth.py &
 
-chromium-browser --kiosk 127.0.0.1:5000
+#open browser
+chromium-browser --noerrdialogs --kiosk --incognito 127.0.0.1:5000
